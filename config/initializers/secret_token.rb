f15a2288aa66dfeb3acb7135c0a9e3f4ab25b6ac
Jerome Basa <jerome@basa.ph>
2014-04-23 18:38:22 -0700

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoTweets::Application.config.secret_key_base = 'c8292000965e914c03bad2baa64cb78a1ea0667820c946bdc2f5a2dda1f705c0e71376ae4028fbe364e69a4c909846b76284f147ce696b3ab10fbb1b291cb5bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MongoTweets::Application.config.session_store :cookie_store, key: '_mongo_tweets_session'
