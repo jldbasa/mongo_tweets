101d9613c8b84fd0f24d6567db2fd9b731f1750f
Jerome Basa <jerome@basa.ph>
2014-04-28 04:28:58 -0700

    move secret_key_base key to ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MongoTweets::Application.config.secret_key_base = 'c8292000965e914c03bad2baa64cb78a1ea0667820c946bdc2f5a2dda1f705c0e71376ae4028fbe364e69a4c909846b76284f147ce696b3ab10fbb1b291cb5bb'
+MongoTweets::Application.config.secret_key_base = Figaro.env.secret_key_base
